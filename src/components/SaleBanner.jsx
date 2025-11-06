4064278ce665de190b0af738d58799d6792121f7
ruthfeezota <ruthzota@gmail.com>
2025-11-06 12:20:53 -0600

    updated sales banner link

--- src/components/SaleBanner.jsx
+++ src/components/SaleBanner.jsx
@@ -6,7 +6,7 @@ function SaleBanner() {
       <div className="absolute top-0 mx-auto flex w-full items-center justify-center bg-[#2C1755] py-4">
         <p className="md:mx-auto text-center text-[#E4CBAA] font-extrabold">
           <a
-            href="https://app.squareup.com/appointments/book/9sa73pd3r58ged/LX4S95WG88DY2/start/UFZKZIVG7Y4RFJ7N3APS7IMZ"
+            href="https://book.squareup.com/appointments/9sa73pd3r58ged/location/LX4S95WG88DY2/services/LQLL6ZTIB5HN5O75TBUYZPGE"
             className="font-extrabold hover:underline"
           >
             🌟 Women Big Box Braids — Only $100! Limited Time. Book Your Spot Now!
